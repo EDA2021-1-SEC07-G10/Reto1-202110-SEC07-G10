c1b12adb585fbf474352ac8b03b1c7b269a73ee0
Ranarcoiris <af.vargasc1@uniandes.edu.co>
2021-03-10 13:53:15 -0500

    Est1 changes

--- App/controller.py
+++ App/controller.py
@@ -50,7 +50,7 @@ def loadData(catalog):
 
 def loadVideos(catalog):
 
-    videosfile = cf.data_dir + 'videos-small.csv'
+    videosfile = cf.data_dir + 'videos-large.csv'
     input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
     for video in input_file:
         model.addVideo(catalog, video)
@@ -62,6 +62,7 @@ def loadCategories(catalog):
     input_file = csv.DictReader(open(categoriesfile, encoding='utf-8'))
     for category in input_file:
         model.addCategory(catalog, category)
+
 
 # Funciones de ordenamiento
 
